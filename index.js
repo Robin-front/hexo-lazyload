31590883d4354049257c44ab48466b0985a37193
robin <lb.robin1991@gmail.com>
2018-03-24 14:54:08 +0800

    async generate thumb and transfer html in after_post_render & add script in after_generate & add css

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 const path = require('path');
 const fs = require('fs');
+const crypto = require('crypto');
+const url = require('url');
 const cheerio = require('cheerio');
 const mkdirp = require('mkdirp');
 const gm = require('gm');
 const request = require('request');
-const crypto = require('crypto');
-const url = require('url');
 const async = require('async');
 
 const config = hexo.config;
@@ -16,20 +16,17 @@ if (!lazyload || !lazyload.enable) {
   return;
 }
 
-const concurrency = lazyload.concurrency || 20;
+const concurrency = lazyload.concurrency || 2;
 const loadingImgPath = lazyload.loadingImg || '/js/lazyload-plugin/loading.svg';
 const thumbPath = lazyload.thumbPath || './images/thumb';
-const thumbTargetFolder = path.resolve(hexo.base_dir, 'public/', thumbPath);
+const thumbTargetFolder = path.resolve(hexo.public_dir, thumbPath);
 const assetsList = ['lazyload.intersectionObserver.min.js', 'loading.svg'];
 
 const error = err => hexo.log.error(err);
 const log = err => hexo.log.info(err);
 const debug = err => hexo.log.debug(err);
-const addScript = (src, content) => content + `<script type="text/javascript" charset="utf-8">!function(){var r=document.querySelectorAll("script");if(Array.prototype.some.call(r,function(r){return r.src&&-1!=r.src.indexOf("${src}")}))return!1;var e=document.createElement("script");e.setAttribute("src","${src}"),document.body.appendChild(e)}();</script>`
-const addStyle = (src, content) => content + `<link rel="stylesheet" href="${src}">`
 
 const isRemotePath = str => str.indexOf('http') !== -1;
-
 const isImg = str => /\/([^/]+\.(?:png|jpg|jpeg|gif|bmp))/i.test(str);
 const getHashFileName = fpath => {
   const extname = path.extname(url.parse(fpath).pathname);
@@ -53,7 +50,7 @@ const copyFileInExist = async (origin, dest) => {
 
 const copyAssets = () => {
   const libPath = path.resolve(__dirname, 'lib');
-  const pluginPath = path.resolve(hexo.base_dir, 'public/js/lazyload-plugin');
+  const pluginPath = path.resolve(hexo.public_dir, 'js/lazyload-plugin');
   for (let file of assetsList) {
     const origin = path.join(libPath, file);
     const dest = path.join(pluginPath, file);
@@ -61,42 +58,24 @@ const copyAssets = () => {
   }
 };
 
-const transformHTML = source => {
+const collectImages = source => {
   const className = lazyload.className;
+  const $ = cheerio.load(source);
+  let waitForThumb = [];
+  let imgArr;
   if (className) {
-    const $ = cheerio.load(source);
-    $(className).each((i, s) => {
-      transformImg($(s));
-    });
-    return $.html();
+    imgArr = $(className)
   } else {
-    return source.replace(/<img([^>]+)?>/igm, s => transformImg(s));
+    imgArr = $('img');
   }
+  imgArr.each((i, s) => {
+    const attr = $(s).attr('src');
+    if (isImg(attr) && waitForThumb.indexOf(attr) === -1) {
+      waitForThumb.push(attr);
+    }
+  });
+  return waitForThumb;
 };
-
-let waitForThumb = [];
-const transformImg = s => {
-  const $ = cheerio.load(s);
-  const $img = typeof s === 'string' ? $('img') : s;
-  const attr = $img.attr('src');
-  if (!isImg(attr)) {
-    return s;
-  }
-  if (waitForThumb.indexOf(attr) === -1) {
-    waitForThumb.push(attr);
-  }
-  $img.attr('src', loadingImgPath);
-  $img.attr('data-original', attr);
-  $img.attr('data-thumb', path.join('/', thumbPath, getHashFileName(attr)));
-  $img.css('position', 'absolute');
-  $img.css('left', '0');
-  $img.css('right', '0');
-  $img.css('z-index', '1');
-
-  const parent = $('<div style="position:relative;"></div>')
-  $img.wrap(parent);
-  return $.html();
-}
 
 const generateThumb = async (originPath) => {
   if (!originPath || !isImg(originPath)) {
@@ -104,24 +83,36 @@ const generateThumb = async (originPath) => {
   }
   const hashFileName = getHashFileName(originPath);
   const targetPath = path.resolve(thumbTargetFolder, hashFileName);
+  const targetRelativePath = path.join(thumbPath, hashFileName);
   const exists = await existFile(targetPath);
   if (!exists) {
-    return await gmAsync(originPath, targetPath);
+    return await gmAsync(originPath, targetPath, targetRelativePath);
   };
 }
 
-const gmAsync = async (originPath, targetPath) => await new Promise((resolve, reject) => {
+const gmAsync = async (originPath, targetPath, targetRelativePath) => await new Promise((resolve, reject) => {
+  let size;
   gm(getOriginImage(originPath))
-    .thumbnail(200, 200)
-    .quality(8)
+    .size(function(err, result){
+      size = result;
+    })
+    .thumbnail(60, 60)
     .write(targetPath, function (err, res) {
       if (err) {
         error('gm: ' + 'fail to generate thumb of ' + originPath);
         error(err);
-        reject(err);
+        reject({
+          originPath,
+          targetPath: targetRelativePath,
+          size: {}
+        });
       } else {
         log('gm: ' + originPath + ' generate thumb successed');
-        resolve(targetPath);
+        resolve({
+          originPath,
+          targetPath: targetRelativePath,
+          size
+        });
       }
     });
 });
@@ -130,31 +121,90 @@ const getOriginImage = originPath => {
   if (isRemotePath(originPath)) {
     return request(originPath);
   } else {
-    return path.join(hexo.base_dir, 'source', originPath);
+    return path.join(hexo.source_dir, originPath);
   }
 }
 
+const dealWithContent = (html, thumbMap) => {
+  const className = lazyload.className;
+  const $ = cheerio.load(html, { decodeEntities: false });
+  let imgArr;
+  if (className) {
+    imgArr = $(className)
+  } else {
+    imgArr = $('img');
+  }
+  imgArr.each((index, el) => {
+    const $el = $(el);
+    const src = $el.attr('src');
+    let thumb;
+    const hasThumb = thumbMap.some((obj) => {
+      if(obj.originPath === src) {
+        thumb = obj;
+        return true;
+      }
+      return false;
+    });
+    if (hasThumb) {
+      const { size, targetPath } = thumb;
+      $el.attr('class', 'progress-images--original')
+        .attr('src', loadingImgPath)
+        .attr('data-original', src)
+        .attr('data-thumb', targetPath);
+      const parent = $(`<div class="progress-images" style="width:${size.width}px"></div>`);
+      $el.wrap(parent);
+      const sizeRate = size.height * 100 / size.width;
+      const placeholder = $(`<div class="progress-images--placeholder" style="padding-bottom: ${sizeRate}%;"></div>`);
+      $el.before(placeholder);
+    }
+  });
+  return $.html();
+}
 hexo.extend.filter.register('before_post_render', async function (data) {
   await mkdir(thumbTargetFolder);
 });
 
 hexo.extend.filter.register('after_post_render', function (data) {
-  copyAssets();
-  data.content = addScript('/js/lazyload-plugin/lazyload.intersectionObserver.min.js', data.content);
-  data.content = transformHTML(data.content);
-  return data;
+  return new Promise(resolve => {
+    let originArray = collectImages(data.content);
+    async.mapLimit(originArray, concurrency, async function (url) {
+      return await generateThumb(url);
+    }, (err, thumbMap) => {
+      if (err) { reject(err) };
+      originArray = null;
+      resolve(thumbMap);
+      log(data.source + 'All thumb process successed !');
+    });
+  }).then((thumbMap) => {
+    data.content = dealWithContent(data.content, thumbMap);
+
+    return data;
+  });
 });
 
-hexo.extend.filter.register('before_exit', async function loopPipe() {
-  if (!waitForThumb.length) { return false; }
-  log('start processing ' + waitForThumb.length +' thumb');
-  return await async.mapLimit(waitForThumb, concurrency, async function (url) {
-    const response = await generateThumb(url);
-    return response;
-  }, (err) => {
-    if (err) throw err;
-    // results is now an array of the response bodies
-    // console.log('results', results)
-    log('All thumb process successed !');
+hexo.extend.filter.register('before_generate', function (data) {
+  copyAssets();
+});
+
+hexo.extend.filter.register('after_generate', function () {
+  const route = hexo.route;
+  var routes = route.list().filter(path => path.endsWith('.html'));
+  const map = routes.map(path => {
+    return new Promise((resolve, reject) => {
+      const html = route.get(path);
+      let htmlTxt = '';
+      html.on('data', (chunk) => (htmlTxt += chunk));
+      html.on('end', () => {
+        const $ = cheerio.load(htmlTxt, { decodeEntities: false });
+        $('body').append(`<script type="text/javascript" charset="utf-8" src="/js/lazyload-plugin/lazyload.intersectionObserver.min.js"></script>`);
+        resolve({path, html: $.html()});
+      });
+    });
   });
-}, 1);
+
+  return Promise.all(map).then((res) => {
+    res.map(obj => {
+      route.set(obj.path, obj.html);
+    });
+  });
+});
